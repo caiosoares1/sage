b2bbeb47cdb17c30525bfc19b88d683d26de7a79
caiosoares1 <batistacaio2244@gmail.com>
2025-11-23 20:06:54 -0300

    feat(admin): register all models in Django Admin
    - Register Usuario and Notificacao with custom fields
    - Register Empresa, Instituicao, Supervisor, CursoCoordenador
    - Register Aluno, Estagio, Avaliacao, Documento
    - Configure list_display for better visualization

--- sage/admin/admin.py
+++ sage/admin/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Instituicao, Empresa, Supervisor, CursoCoordenador
 
-# Register your models here.
+@admin.register(Instituicao)
+class InstituicaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'contato', 'rua', 'numero', 'bairro')
+    search_fields = ('nome', 'contato')
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('razao_social', 'cnpj', 'rua', 'numero', 'bairro')
+    search_fields = ('razao_social', 'cnpj')
+
+@admin.register(Supervisor)
+class SupervisorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'contato', 'empresa')
+    list_filter = ('empresa',)
+    search_fields = ('nome', 'cargo')
+
+@admin.register(CursoCoordenador)
+class CursoCoordenadorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nome_curso', 'codigo_curso', 'contato', 'instituicao')
+    list_filter = ('instituicao',)
+    search_fields = ('nome', 'nome_curso')
--- sage/estagio/admin.py
+++ sage/estagio/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Aluno, Estagio, Avaliacao, Documento
 
-# Register your models here.
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'contato', 'instituicao')
+    search_fields = ('nome', 'matricula')
+    list_filter = ('instituicao',)
+
+@admin.register(Estagio)
+class EstagioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'cargo', 'empresa', 'supervisor', 'data_inicio', 'data_fim', 'status')
+    list_filter = ('status', 'empresa')
+    search_fields = ('titulo', 'cargo')
+    date_hierarchy = 'data_inicio'
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('estagio', 'supervisor', 'nota', 'data_avaliacao')
+    list_filter = ('data_avaliacao',)
+    search_fields = ('estagio__titulo',)
+
+@admin.register(Documento)
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ('nome_arquivo', 'tipo', 'estagio', 'coordenador', 'data_envio', 'versao')
+    list_filter = ('tipo', 'data_envio')
+    search_fields = ('nome_arquivo', 'estagio__titulo')
