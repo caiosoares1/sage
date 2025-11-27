1c9b51aef8b27c568e8c14643b30443d608c6c31
caiosoares1 <batistacaio2244@gmail.com>
2025-11-26 22:25:53 -0300

    feat: adicionar rotas para listar e aprovar solicitações no coordenador

--- sage/admin/urls_coordenador.py
+++ sage/admin/urls_coordenador.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'coordenador'
 
 urlpatterns = [
+    path('solicitacoes/', views.listar_solicitacoes_coordenador, name='listar_solicitacoes_coordenador'),
+    path('aprovar/<int:estagio_id>/', views.aprovar_estagio, name='aprovar_estagio'),
+    path('reprovar/<int:estagio_id>/', views.reprovar_estagio, name='reprovar_estagio'),
     path('documentos/', views.aprovar_documentos_coordenador, name='documentos'),
     path('documentos/<int:documento_id>/aprovar/', views.aprovar_documento_coordenador, name='aprovar_documento'),
 ]
