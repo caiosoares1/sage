4b448b5fef04d9e450e293953e5b3bb730a8da09
Bruno Eneas <brunovn.eneas@gmail.com>
2025-12-16 21:53:24 -0300

    feat: allow all framing options by setting X_FRAME_OPTIONS to 'ALLOWALL'

--- sage/sage/settings.py
+++ sage/sage/settings.py
@@ -154,3 +154,5 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', f'SAGE <{EMAIL_HOST_USER}>')
+
+X_FRAME_OPTIONS = 'ALLOWALL'
